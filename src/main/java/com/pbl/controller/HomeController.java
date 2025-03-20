79bfad4b50b68f3be72a3e7249b3e0ce9c438d5d
shrihari7396 <shriharik121@gmail.com>
2025-03-20 17:47:42 +0530

    Code written By Shrihari Kulkarni

--- src/main/java/com/pbl/controller/HomeController.java
+++ src/main/java/com/pbl/controller/HomeController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String homePage() {
-        int[] arr = new int[0];
-        int length = arr.length;
         return "This is Home Page!!!";
     }
 }
