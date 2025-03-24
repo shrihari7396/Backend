ea3e504a25032ad233850cdcb4f777c94e50ed88
shrihari7396 <shriharik121@gmail.com>
2025-03-24 15:59:52 +0530

    Trail Purpose

--- src/main/java/com/pbl/config/CorsConfig.java
+++ src/main/java/com/pbl/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.pbl.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +13,8 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@Nullable CorsRegistry registry) {
+                assert registry != null;
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
